3d10ba24a4424fa6849e18423866fb672ab038ce
vinod Amariyil <kevinparackal10@gmail.com>
2023-02-17 23:41:39 +0530

    artk

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { data } from 'src/assets/data';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent {
+artform = data
+ngOninit():void{
+  this.artform=data;
+}
+}
---
+++ src/assets/data.ts
@@ -0,0 +1,62 @@
+export const data=[
+    {id:1,
+        name:'ODISSI',
+        Image:'odisi2.jpg',
+        desc:''
+
+    },
+    {id:2,
+        name:'kathak',
+        Image:'kathak.jpg',
+        desc:''
+
+    },
+    {id:3,
+        name:'kadhakali',
+        Image:'katha2.jpg',
+        desc:''
+
+    },
+    {id:4,
+        name:'kadhakali',
+        Image:'sangeetnatak.JPG.jpg',
+        desc:''
+
+    },
+    {id:5,
+        name:'kadhakali',
+        Image:'kalaripayt.jpg',
+        desc:''
+
+    },
+    {id:6,
+        name:'kadhakali',
+        Image:'kalaripayt.jpg',
+        desc:''
+
+    },
+    {id:7,
+        name:'kadhakali',
+        Image:'kalaripayt.jpg',
+        desc:''
+
+    },
+    {id:8,
+        name:'kadhakali',
+        Image:'kalaripayt.jpg',
+        desc:''
+
+    },
+    {id:9,
+        name:'kadhakali',
+        Image:'kalaripayt.jpg',
+        desc:''
+
+    },
+    {id:10,
+        name:'kadhakali',
+        Image:'kalaripayt.jpg',
+        desc:''
+
+    }
+]
